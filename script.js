7601569c1a0e956e0d135bfb42797bd20e5c97f2
Andrew Kozinski <andrewkozinski76@gmail.com>
2024-09-05 23:42:00 -0400

    Fetch set to fetch from github page

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-fetch('https://server.com/data.json')
+fetch('https://raw.githubusercontent.com/andrewkozinski/andrewkozinski.github.io/main/projects.json')
     .then((response) => response.json())
     .then((json) => console.log(json));
