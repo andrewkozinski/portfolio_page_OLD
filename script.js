b43d4845a86571b4c1c9449c85cc6d9cb67d292d
Andrew Kozinski <andrewkozinski76@gmail.com>
2024-09-06 00:13:47 -0400

    Added icon for FSC, JS to align FSC icon

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const schoolAttended = document.getElementById("schoolAttended");
+
+document.addEventListener('DOMContentLoaded', () => {
+    schoolAttended.style.left = `${document.getElementById("heading").offsetWidth - 700}px`;
+    console.log("Width: " + document.getElementById("heading").offsetWidth);
+});
+
+//handle JSON projects content
 fetch('https://raw.githubusercontent.com/andrewkozinski/andrewkozinski.github.io/main/projects.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((data) => {
+        console.log(data);
+
+    });
