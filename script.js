b8845e3fb42fe17ccdbf132d665ef34606e17854
andrewkozinski <andrewkozinski76@gmail.com>
2024-09-06 16:35:20 -0400

    Playing around with left styling on school attended div

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const schoolAttended = document.getElementById("schoolAttended");
 
 document.addEventListener('DOMContentLoaded', () => {
-    schoolAttended.style.left = `${document.getElementById("right-content").offsetWidth - 700}px`;
+    schoolAttended.style.left = `${document.getElementById("right-content").offsetWidth}px`;
     schoolAttended.style.top = `${document.getElementById("heading").offsetHeight * .25}px`
-    console.log("Width: " + document.getElementById("heading").offsetWidth);
+    console.log("Width: " + document.getElementById("right-content").clientLeft);
 });
 
 //handle JSON projects content
